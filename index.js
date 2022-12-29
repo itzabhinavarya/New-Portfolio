9ef2b754f151e3bb86663e150152620b7b8c9a42
Abhinav Arya <itzabhinavarya@gmail.com>
2022-12-29 16:40:12 +0530

    New Files

---
+++ index.js
@@ -0,0 +1,7 @@
+function welcome(){
+    var date = new Date();
+    window.alert("Welcome To My Portfolio\n\n"+date);
+}
+function contactUs(){
+    window.alert("Contact Me Only For Valid Purpose....\nPlease Don't Spam...");
+}
